4a9d399ee2e26cc6fde4835728dd13e6742d6a05
arikarim <arikarim146@gmail.com>
2021-08-07 23:44:18 +0300

    Add image to profile

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,32 +1,43 @@
 import { Container } from "@material-ui/core";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import image from "../images/react.png";
 import "./profile.css";
 import "./pro.css";
 import { Link } from "react-router-dom";
+import axios from "axios";
 const Profile = () => {
+  const [userr, setUserr] = useState(null);
   const user = JSON.parse(localStorage.getItem("user"));
+  const fetchuser = async () => {
+    const res = await axios.get("http://localhost:3001/users/index");
+    const correct = res.data.filter((item) => item.id === user.id);
+    setUserr(correct[0]);
+  };
+  useEffect(() => {
+    fetchuser();
+  }, [userr]);
   return (
     <Container className="cont bg-light">
       <Row>
         <Col className="mx-auto d-flex flex-column align-content-center" lg={6}>
           <img
             className="user-image mx-auto my-2"
-            src={user.image_data || image}
+            src={
+              (userr && userr.image_data.metadata.cloudinary.secure_url) ||
+              image
+            }
             alt="avatar"
           />
-          <h1 className="text-dark text-center">{user.bio || "Stranger"}</h1>
-          <p className=" text-center">{user.about || "Not available "}</p>
+          <h1 className="text-dark text-center">
+            {(userr && userr.bio) || "Stranger"}
+          </h1>
+          <p className=" text-center">
+            {(userr && userr.about) || "Not available "}
+          </p>
           <Link className="btn btn-light" to="/editprofile">
             Edit profile
           </Link>
-          {/* <div className="progress__chart__text">
-            <span className="txt">You achieved</span>
-            <span className="num">60</span>
-            <span className="txt">on average</span>
-          </div>
-          <div className="progress__chart__container"></div> */}
         </Col>
       </Row>
     </Container>
