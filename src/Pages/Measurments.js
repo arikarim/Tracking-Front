234db1b25eeb9507a772d4f0967c495f4407174d
arikarim <arikarim146@gmail.com>
2021-08-08 11:12:09 +0300

    Improve measurments

--- src/Pages/Measurments.js
+++ src/Pages/Measurments.js
@@ -8,6 +8,9 @@ import dateHandle from "../PureFunctions/time";
 
 const Measurments = () => {
   const [data, setData] = useState([]);
+  const [today, setToday] = useState([]);
+  const [yesterday, setYesterday] = useState([]);
+  const [lastWeek, setLastWeek] = useState([]);
   const history = useHistory();
   const user = JSON.parse(localStorage.getItem("user"));
   const id = history.location.pathname.split("/")[2];
@@ -18,6 +21,15 @@ const Measurments = () => {
       const res = data.data.filter(
         (d) => d.user_id === user.id && d.measure_id === Number(id)
       );
+      setToday(res.filter((item) => dateHandle(item.date, final) < 29));
+      setYesterday(
+        res.filter(
+          (item) =>
+            dateHandle(item.date, final) > 29 &&
+            dateHandle(item.date, final) <= 31
+        )
+      );
+      setLastWeek(res.filter((item) => dateHandle(item.date, final) > 31));
       // console.log(res);
       setData(res);
     } catch (e) {
@@ -31,85 +43,75 @@ const Measurments = () => {
     <Container className="cont">
       <h1 className="mx-auto text-center">Measurments</h1>
       <Col>
-        <h5>Today:</h5>
-        {data &&
-          data
-            .filter((item) => dateHandle(item.date, final) < 29)
-            .map((m) => (
-              <Link
-                className="my-auto text-decoration-none"
-                key={m.id}
-                to={`/${name}/show/${m.id}`}
-              >
-                <Card className="my-2">
-                  <Card.Body className="d-flex justify-content-between">
-                    <Card.Text className="my-auto text-decoration-none link-dark">
-                      {name}
-                    </Card.Text>
-                    <Card.Text className="my-auto text-decoration-none link-dark">
-                      {m.number}
-                    </Card.Text>
-                    <Card.Text className="my-auto text-decoration-none link-dark">
-                      {m.date}
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Link>
-            ))}
-        <h5>Yesterday</h5>
-        {data &&
-          data
-            .filter(
-              (item) =>
-                dateHandle(item.date, final) > 29 &&
-                dateHandle(item.date, final) < 31
-            )
-            .map((m) => (
-              <Link
-                className="my-auto text-decoration-none"
-                key={m.id}
-                to={`/${name}/show/${m.id}`}
-              >
-                <Card className="my-2">
-                  <Card.Body className="d-flex justify-content-between">
-                    <Card.Text className="my-auto text-decoration-none link-dark">
-                      {name}
-                    </Card.Text>
-                    <Card.Text className="my-auto text-decoration-none link-dark">
-                      {m.number}
-                    </Card.Text>
-                    <Card.Text className="my-auto text-decoration-none link-dark">
-                      {m.date}
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Link>
-            ))}
-        <h5>Last week and before</h5>
-        {data &&
-          data
-            .filter((item) => dateHandle(item.date, final) > 30)
-            .map((m) => (
-              <Link
-                className="my-auto text-decoration-none"
-                key={m.id}
-                to={`/${name}/show/${m.id}`}
-              >
-                <Card className="my-2">
-                  <Card.Body className="d-flex justify-content-between">
-                    <Card.Text className="my-auto text-decoration-none link-dark">
-                      {name}
-                    </Card.Text>
-                    <Card.Text className="my-auto text-decoration-none link-dark">
-                      {m.number}
-                    </Card.Text>
-                    <Card.Text className="my-auto text-decoration-none link-dark">
-                      {m.date}
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Link>
-            ))}
+        {today.length > 0 && <h5>Today:</h5>}
+        {today.length > 0 &&
+          today.map((m) => (
+            <Link
+              className="my-auto text-decoration-none"
+              key={m.id}
+              to={`/${name}/show/${m.id}`}
+            >
+              <Card className="my-2">
+                <Card.Body className="d-flex justify-content-between">
+                  <Card.Text className="my-auto text-decoration-none link-dark">
+                    {name}
+                  </Card.Text>
+                  <Card.Text className="my-auto text-decoration-none link-dark">
+                    {m.number}
+                  </Card.Text>
+                  <Card.Text className="my-auto text-decoration-none link-dark">
+                    {m.date}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Link>
+          ))}
+        {yesterday.length > 0 && <h5>Yesterday</h5>}
+        {yesterday.length > 0 &&
+          yesterday.map((m) => (
+            <Link
+              className="my-auto text-decoration-none"
+              key={m.id}
+              to={`/${name}/show/${m.id}`}
+            >
+              <Card className="my-2">
+                <Card.Body className="d-flex justify-content-between">
+                  <Card.Text className="my-auto text-decoration-none link-dark">
+                    {name}
+                  </Card.Text>
+                  <Card.Text className="my-auto text-decoration-none link-dark">
+                    {m.number}
+                  </Card.Text>
+                  <Card.Text className="my-auto text-decoration-none link-dark">
+                    {m.date}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Link>
+          ))}
+        {lastWeek.length > 0 && <h5>Last week and before</h5>}
+        {lastWeek.length > 0 &&
+          lastWeek.map((m) => (
+            <Link
+              className="my-auto text-decoration-none"
+              key={m.id}
+              to={`/${name}/show/${m.id}`}
+            >
+              <Card className="my-2">
+                <Card.Body className="d-flex justify-content-between">
+                  <Card.Text className="my-auto text-decoration-none link-dark">
+                    {name}
+                  </Card.Text>
+                  <Card.Text className="my-auto text-decoration-none link-dark">
+                    {m.number}
+                  </Card.Text>
+                  <Card.Text className="my-auto text-decoration-none link-dark">
+                    {m.date}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Link>
+          ))}
       </Col>
     </Container>
   );
